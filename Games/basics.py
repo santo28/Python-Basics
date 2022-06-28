1bd053931a8099845e803c854b22070e57130d21
Eldersantos17! <san17004@byui.edu>
2022-06-27 18:13:49 -0600

    python basics

---
+++ Games/basics.py
@@ -0,0 +1,27 @@
+# Basics of Python for new programmers 
+ 
+#1 Printing in Python
+print("Hello, Class!")
+
+#2 Syntax and if-statement. INDENTATION IS IMPORTANT IF NOT YOU WILL GET AN ERROR 
+if 5 > 2: 
+    print ('Team #6 is the BEST')
+
+
+#3 Indentation has to be consistent 
+
+if 4 > 2:
+    print("Indentation is Important") 
+if 3 > 2:
+    print("Don't forget to use the same amount of identation in the same Code Block!") 
+
+#4 Storing Variables 
+x = 5
+y = "John"
+print(x)
+print(y)   
+
+#5 Variables do not need to be declared with any particular type, and can even change type after they have been set.
+x = 4       # x is of type integer
+x = "Sally" # BUT NOW WE ARE SAYING THAT --> x is now of type string
+print(x)
